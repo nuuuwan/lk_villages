2a0ad31e89a7259705fcec194a79b675de85b67a
nuuuwan <nuuuwan@gmail.com>
2023-12-03 11:23:06 +0530

    n_file_size

--- workflows/update_readme.py
+++ workflows/update_readme.py
@@ -16,25 +16,28 @@ def main():
     n_dsds = 0
     n_gnds = 0
     n_villages = 0
+    n_file_size = 0
     for file_only in os.listdir(DIR_DATA_VILLAGES):
         file_only_list.append(file_only)
         file_path = os.path.join(DIR_DATA_VILLAGES, file_only)
-
+        file_size = os.path.getsize(file_path)
+        n_file_size += file_size
         data = JSONFile(file_path).read()
         n_dsds += 1
         n_gnds_for_dsd = len(data['children'])
         n_gnds += n_gnds_for_dsd
         for gnd in data['children']:
-            n_villages_for_gnd = len(gnd.get('children',[]))
+            n_villages_for_gnd = len(gnd.get('children', []))
             n_villages += n_villages_for_gnd
 
+    n_file_size_m = n_file_size / 1_000_000
     lines = [
         '# Villages of Sri Lanka',
         '',
         'Scrapes information about villages from http://moha.gov.lk:8090.',
         '',
         f'**{n_villages}** Villages from **{n_gnds}** GNDs'
-        + f' in **{n_dsds}** DSDs, scraped as of *{time_str}*.',
+        + f' in **{n_dsds}** DSDs ({n_file_size_m:.1f}MB), scraped as of *{time_str}*.',
     ]
 
     previous_province_id = None
